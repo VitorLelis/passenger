4e364b9b67ba00b090fd3d38a03b79cebd80b9ab
VitorLelis <vitorll9090@gmail.com>
2024-02-05 23:28:09 +0000

    feat(response): added password analyze

--- src/response.py
+++ src/response.py
@@ -1,6 +1,6 @@
 from secrets import choice
 from string import ascii_lowercase, ascii_uppercase, digits
-from re import compile
+from re import compile,sub
 
 def generate(message: str) -> str:
     flags = {'-lower': ascii_lowercase, 
@@ -28,11 +28,57 @@ def generate(message: str) -> str:
 
     return ''.join(choice(chars) for i in range(size)) 
 
+def char_score(message: str) -> int:
+    lower = compile(r'[a-z]')
+    upper = compile(r'[A-Z]')
+    digits = compile(r'[0-9]')
+    symbols = compile(r'[\!\@\#\$\%\^\(\)]')
+
+    score = 0
+
+    if lower.search(message):
+        score += 1
+    if upper.search(message):
+        score += 1
+    if digits.search(message):
+        score += 1
+    if symbols.search(message):
+        score += 1
+    
+    return score
+
+def analyze(message: str) -> str:
+    strength = {
+        0 : 13*[0],
+        1 : 2*[0] + 6*[1] + 5*[2],
+        2 :   [0] + 5*[1] + 7*[2],
+        3 : 5*[1] + 8 *[2],
+        4 : 5*[1] + 8 *[2]
+    }
+
+    msg = sub('=>','', message)
+
+    score = char_score(msg)
+
+    size = len(msg)
+
+    score = 0 if size < 6 else char_score(msg)
+
+    size_param = min(size-6, 12)
+
+    result = strength[score][size_param]
+
+    answers = ['weak', 'moderate', 'strong']
+
+    return 'Your password is '+ answers[result]
+
 def talk(message: str) -> str:
     genrgx = compile(r'(\!\d+|\-lower|\-upper|\-digits|\-symbols)')
     
     if genrgx.search(message):
         return generate(message)
+    elif message.startswith('=>'):
+        return analyze(message)
     elif message == 'Hello there':
         return '_General Kenobi_'
     else:
